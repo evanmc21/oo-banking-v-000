1bdea88fa3d08699ded4052aeb85cc291ff672b0
Evan Candler <emcandler21@gmail.com>
2018-03-06 03:56:07 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,5 +2,11 @@ class Transfer
   attr_accessor :status
   attr_reader :sender, :receiver, :amount
 
-  
+  def initialize
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = status
+
+  end
 end
